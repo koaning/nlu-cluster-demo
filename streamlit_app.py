6d45b7ea1fb21fc300bf57b83821ac2ef688f9fd
Vincent <vincentwarmerdam@gmail.com>
2020-10-16 17:48:49 +0200

    upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pathlib 
+from io import StringIO
 
 import streamlit as st
 import matplotlib.pylab as plt
@@ -6,8 +7,15 @@ from sklearn.cluster import AgglomerativeClustering
 from whatlies.language import CountVectorLanguage
 from whatlies.transformers import Pca, Umap
 
-txt = pathlib.Path("nlu.md").read_text()
-texts = [t.replace(" - ", "") for t in txt.split("\n") if len(t) > 0 and t[0] != "#"]
+uploaded = st.sidebar.file_uploader("Upload a File")
+if not uploaded:
+    txt = pathlib.Path("nlu.md").read_text()
+    texts = [t.replace(" - ", "") for t in txt.split("\n") if len(t) > 0 and t[0] != "#"]
+else:
+    bytes_data = uploaded.read()
+    stringio = StringIO(bytes_data.decode("utf-8"))
+    string_data = stringio.read()
+    texts = [t.replace(" - ", "") for t in string_data.split("\n") if len(t) > 0 and t[0] != "#"]
 
 n_svd = st.sidebar.slider("Number of SVD components", min_value=2, max_value=100, step=1)
 min_ngram, max_ngram = st.sidebar.slider("Range of ngrams", min_value=1, max_value=5, step=1, value=(2, 3))
@@ -23,7 +31,8 @@ if cluster:
     linkage = st.sidebar.selectbox(
         'Linkage Method',
         ('average', 'complete', 'ward', 'single'))
-    n_cluster = st.sidebar.slider("Number of clusters", min_value=2, max_value=100, step=1)
+    n_cluster = st.sidebar.slider("Number of clusters", min_value=2, max_value=100, step=1, value=20)
+    
     model = AgglomerativeClustering(linkage=linkage, n_clusters=n_cluster)
     X = embset.to_X()
     clusters = {n: str(c) for n, c in zip([e.name for e in embset], model.fit_predict(X))}
