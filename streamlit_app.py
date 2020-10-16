b136bc3dd1bc1781bec1469540494df3265ce2aa
Vincent <vincentwarmerdam@gmail.com>
2020-10-16 16:55:34 +0200

    v0

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,13 @@
 import numpy as np
 import streamlit as st
 import matplotlib.pylab as plt
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
+import pathlib 
+from whatlies.language import CountVectorLanguage
+from whatlies.transformers import Pca, Umap
 
-n = 1000
-np.random.seed(42)
-x = np.linspace(0, 6, n)
-X = np.linspace(0, 6, n)[:, np.newaxis]
-y = np.sin(X).ravel() + np.sin(6 * X).ravel() + np.random.random(n) * 0.3
+txt = pathlib.Path("nlu.md").read_text()
+texts = [t.replace(" - ", "") for t in txt.split("\n") if len(t) > 0 and t[0] != "#"]
 
-n_est = st.sidebar.slider("n_est", min_value=1, max_value=5_000, step=1)
+lang = CountVectorLanguage(10)
 
-@st.cache
-def make_predictions(n_est):
-    mod1 = DecisionTreeRegressor(max_depth=4)
-    y1 = mod1.fit(X,y).predict(X)
-    y2 = AdaBoostRegressor(mod1, n_estimators=n_est).fit(X, y).predict(X)
-    return y1, y2
-
-y1, y2 = make_predictions(n_est=n_est)
-
-if st.sidebar.checkbox("Toggle ScatterChart"):
-    plt.scatter(x, y, alpha=0.1)
-plt.plot(x, y1, label="just a tree")
-plt.plot(x, y2, label=f"adaboost-{n_est}")
-plt.legend()
-
-st.pyplot()
+st.write(lang[texts].transform(Umap(2)).plot_interactive().properties(width=500, height=500))
