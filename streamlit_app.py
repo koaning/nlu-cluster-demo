1939f80325ecaa80e5920daed06ad3c6e6a5d4d2
Vincent <vincentwarmerdam@gmail.com>
2020-10-16 17:11:29 +0200

    better!

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,18 @@ from whatlies.transformers import Pca, Umap
 txt = pathlib.Path("nlu.md").read_text()
 texts = [t.replace(" - ", "") for t in txt.split("\n") if len(t) > 0 and t[0] != "#"]
 
-lang = CountVectorLanguage(10)
+n_svd = st.sidebar.slider("Number of SVD components", min_value=2, max_value=100, step=1)
+min_ngram, max_ngram = st.sidebar.slider("Range of ngrams", min_value=1, max_value=5, step=1, value=(2, 3))
 
-st.write(lang[texts].transform(Umap(2)).plot_interactive().properties(width=500, height=500))
+
+st.markdown("# Simple Text Clustering")
+lang = CountVectorLanguage(n_svd, ngram_range=(min_ngram, max_ngram))
+
+p = (lang[texts]
+     .transform(Umap(2))
+     .plot_interactive(annot=False)
+     .properties(width=500, height=500))
+
+st.write(p)
+
+
